d6d2a8bc0f947ab4a32e79e51056fbbc76be1523
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-04-23 03:32:49 +0400

    Add script to 0-safe_print_list.py and created README.md

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """
+    Prints x elements of a list on the same line, followed by a new line.
+    Args:
+        my_list: The list to print elements from.
+        x: The number of elements to print.
+    Returns:
+        The real number of elements printed.
+    """
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            break
+    print("")
+    return count
