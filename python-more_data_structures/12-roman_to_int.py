a5af5a8425555a8cbbf0bee8123940d0f679dde6
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-04-23 03:26:17 +0400

    Add script to 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -14,13 +14,10 @@ def roman_to_int(roman_string):
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 
         'C': 100, 'D': 500, 'M': 1000
     }
-    
     total = 0
     length = len(roman_string)
-    
     for i in range(length):
         current_val = roman_dict.get(roman_string[i], 0)
-        
         # If there is a next character and the current value is less than the next,
         # it means we are in a subtraction scenario (e.g., IV or IX)
         if i + 1 < length and current_val < roman_dict.get(roman_string[i + 1], 0):
