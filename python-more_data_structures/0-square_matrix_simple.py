8a0ac425e81caf0f2b9799b069a9b78c2e20db2e
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-04-23 02:55:46 +0400

    Added script to 0-square_matrix_simple.py and created README.md

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    """
+    Computes the square value of all integers of a matrix.
+    
+    Args:
+        matrix: A 2-dimensional array (list of lists) of integers.
+        
+    Returns:
+        A new matrix of the same size with squared values.
+    """
+    return [[x ** 2 for x in row] for row in matrix]
