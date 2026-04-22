ac295855db440c9d6dd281dbd3e692192b9020c6
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-04-23 02:59:22 +0400

    Add script to 1-search_replace.py

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """
+    Replaces all occurrences of an element by another in a new list.
+    Args:
+        my_list: The initial list.
+        search: The element to replace in the list.
+        replace: The new element.
+    Returns:
+        A new list with the elements replaced.
+    """
+    return [replace if x == search else x for x in my_list]
