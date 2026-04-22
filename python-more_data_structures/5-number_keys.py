4f093deeba2a3ce62f24a22aa2c570e0e67dfef6
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-04-23 03:09:48 +0400

    Add script to 5-number_keys.py

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+    Args:
+        a_dictionary: The dictionary to count keys from.
+    Returns:
+        The number of keys (integer).
+    """
+    return len(a_dictionary)
