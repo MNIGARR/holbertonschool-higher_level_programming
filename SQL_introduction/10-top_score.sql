af0f1bd8f6f7c24c6d5d8d169ff70e200a237ce5
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-05-03 18:54:05 +0400

    Add SQL script to list records from second_table sorted by score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- List all records of the table second_table of the database.
+-- The records should be sorted by score (highest score first).
+SELECT name, score
+FROM second_table
+ORDER BY score DESC;
