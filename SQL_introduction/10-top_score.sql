dba6cd7946008353a8722e552726081d969ca120
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-05-03 18:56:23 +0400

    Update SQL script to correct column order and enhance comments for clarity

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
--- List all records of the table second_table of the database.
--- The records should be sorted by score (highest score first).
-SELECT name, score
+-- Lists all records of the table second_table.
+-- Displays score and name (in that order) sorted by top score.
+SELECT score, name
 FROM second_table
 ORDER BY score DESC;
