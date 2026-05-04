0ed618e136c03af5fe1337879ec47480dbbc8413
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-05-04 19:44:47 +0400

    Add a blank line for improved readability in model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """State class linked to states table"""
     __tablename__ = 'states'
