00d944d52a5680b527b4c191aab91b94674c9f99
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-05-04 18:52:16 +0400

    Update script to list states starting with 'N' and improve database connection handling

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa"""
+"""Lists all states with a name starting with N"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    )
+    for row in cursor.fetchall():
         print(row)
-    cur.close()
-    conn.close()
+    cursor.close()
+    db.close()
