0f7a7feba13ea90e856a494a0fe489762b69d5a4
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-05-17 19:19:45 +0400

    Fix: improve docstring formatting in is_kind_of_class function; Add: create BaseGeometry class with area method

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
-    Module that defines a function to check if an object is
-    an instance of a class that inherited (directly or indirectly) from the specified class.
+    Module that defines a function to check if an 
+    object is an instance of a class that inherited 
+    (directly or indirectly) from the specified class.
 """
 
 
 def is_kind_of_class(obj, a_class):
     """
-    Return True if obj is an instance of a_class or a class that inherited from a_class,
+    Return True if obj is an instance of a_class or 
+    a class that inherited from a_class,
     otherwise return False.
     """
     return isinstance(obj, a_class)
---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""geometry"""
+
+
+class BaseGeometry:
+    """class"""
+
+    def area(self):
+        """exception"""
+        raise Exception("area() is not implemented")
