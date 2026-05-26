bc4979a36a1729b8bfc407fcb061c6ea98faad1d
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-05-26 17:40:49 +0400

    Add: implement print_matrix_integer function to print a matrix of integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    keys = list(a_dictionary.keys())
-    keys.sort()
-    for key in keys:
-        print("{}: {}".format(key, a_dictionary[key]))
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
+        return
+
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]))
+            else:
+                print("{:d}".format(row[i]), end=" ")
