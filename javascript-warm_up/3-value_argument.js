6dbfc394caa6e16507fe4cea132f80e24a0422bb
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-05-28 00:49:53 +0400

    refactor: standardize variable naming and formatting in 3-value_argument.js

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const firstArg = process.argv[2];
-if (firstArg===undefined){
-    console.log("No Argument");
+const arg = process.argv[2];
+
+if (arg === undefined) {
+  console.log('No argument');
 } else {
-    console.log(firstArg);
+  console.log(arg);
 }
