3e609822b21f34bf3bd1a81cd4d21a0e81cf8e98
MNIGARR <nigarrmustafazadeh@gmail.com>
2026-05-31 01:22:27 +0400

    Add: implement add_attribute function to dynamically add attributes to objects

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module that defines a function to add attributes to objects."""
+
+
+def add_attribute(obj, attribute, value):
+    """
+    Adds a new attribute to an object if it's possible.
+    
+    Args:
+        obj (any): The object to add the attribute to.
+        attribute (str): The name of the attribute to add.
+        value (any): The value of the attribute.
+        
+    Raises:
+        TypeError: If the object cannot have new attributes added.
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attribute, value)
